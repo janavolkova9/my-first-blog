8743f0801b5f31c7eb22f36cfd8edb4e146dd79e
Jana <jana.volkova@gmail.com>
2015-11-28 13:06:15 +0100

    My Django Girls app, firsst commit

---
+++ python_into.py
@@ -0,0 +1,27 @@
+print('Hello, Django girls!')
+name = "Jana"
+print(name)
+if 3>2:
+	print('It works!')
+	
+if 5 > 2:
+	print('5 is indeed greater than 2')
+else:
+		print('5 is not greater than 2')
+		
+def hi():
+	print('Hi there!')
+	print('How are you?')
+hi()
+
+def hi(name):
+	print('Hi ' + name + '!')
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+	hi(name)
+	print('Next girl')
+for i in range(1,6):
+	print(i)
+
+	
